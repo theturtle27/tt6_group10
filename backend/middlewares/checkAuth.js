0ffc5de15c176fbe0b1a2befc073fb0b2a154b9e
Sharon Chan <sharoncchan@users.noreply.github.com>
2022-07-22 15:39:15 +0800

    added auth

---
+++ backend/middlewares/checkAuth.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const checkAuth = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      return next(new Error("Not authorized, token failed"));
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    return next(new Error("Not authorized, no token"));
+  }
+};
