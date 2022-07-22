3269ec8fc4d84e03bdf2349036754ef2cbdf5d5f
theturtle27 <62739849+theturtle27@users.noreply.github.com>
2022-07-22 14:48:27 +0800

    added axios postfor login and register

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Login extends Component {
+  login = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", {
+        username: document.getElementById("username").value,
+        pwd: document.getElementById("password").value,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   render() {
     return (
       <div className="w3-card-4" style={{ margin: "2rem" }}>
         <div className="w3-container w3-blue w3-center w3-xlarge">LOGIN</div>
         <div className="w3-container">
-          <form>
+          <form onSubmit={this.login}>
             <p>
-              <label htmlFor="email">Email</label>
-              <input type="email" class="w3-input w3-border" id="email" />
+              <label htmlFor="username">Username</label>
+              <input type="text" className="w3-input w3-border" id="username" />
             </p>
             <p>
               <label htmlFor="password">Password</label>
-              <input type="password" class="w3-input w3-border" id="password" />
+              <input
+                type="password"
+                className="w3-input w3-border"
+                id="password"
+              />
             </p>
             <p>
               <button type="submit" class="w3-button w3-blue">
